b54e91c7e5a5ba70eded915ce1c3bfbb00ab9dc4
Anton Zaroutski <anton@zaroutski.com>
2016-04-08 23:03:14 +1000

    Adding a route for suggesting a venue

--- htdocs/content/themes/timeplannr/resources/routes.php
+++ htdocs/content/themes/timeplannr/resources/routes.php
@@ -15,6 +15,7 @@
 Route::get( 'page', array( 'anton', 'uses' => 'VenueController@anton') );
 
 Route::get( 'page', array( 'venue/find', 'uses' => 'VenueController@find') );
+Route::any( 'page', array( 'suggest', 'uses' => 'VenueController@suggest') );
 Route::any( 'page', array( 'book', 'uses' => 'VenueController@plan') );
 Route::any( 'page', array(
 
